9b7d5077f39fc1be2b928c07a45598338ca3e0a8
Slaksy <d89049595686t@yandex.ru>
2025-07-24 22:48:14 +0300

    fix mr1

--- TodoListApp/AppDelegate.swift
+++ TodoListApp/AppDelegate.swift
@@ -32,7 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        
     }
 }
 
